f1c56462d541aa7634f13053c60aaf77328bfdf8
Lars <mrgrandmother@gmail.com>
2011-09-07 19:50:00 +0200

    add New un-implemented test classes with matching TODO

---
+++ brunson/src/tests/GameManagerTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameManagerTest {
+
+	@Test
+	public void testPlayGames() {
+		//TODO Add tests for this.
+		fail("Not yet implemented");
+	}
+
+}
