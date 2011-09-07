c7aa301e93725cfbbc8dfbad71381f859f822408
Lars <mrgrandmother@gmail.com>
2011-09-07 16:54:01 +0200

    Added action list to Player for use with contexts.

--- brunson/src/player/Player.java
+++ brunson/src/player/Player.java
@@ -1,15 +1,23 @@
 package player;
 import manager.Round;
 import cards.*;
+import java.util.ArrayList;
 
 public abstract class Player {
 	
 	private Pile hand;
 	private int chips;
+	private ArrayList<Action>[] actions;
 		
 	public Player(int buyin){
 		this.hand = new Pile();
 		chips = buyin;
+		this.actions = new ArrayList[4];
+		//List of actions this player took pre-flop, on flop, and on river.
+		this.actions[0] = new ArrayList<Action>();
+		this.actions[1] = new ArrayList<Action>();
+		this.actions[2] = new ArrayList<Action>();
+		this.actions[3] = new ArrayList<Action>();
 	}
 	
 	public void addCard(Card card) {
@@ -33,5 +41,18 @@ public abstract class Player {
 	}
 	
 	public abstract Action act(Round round, Pile communityCards, int bet, int raises, int pot);
+	
+	public ArrayList<Action> getPreFlopActions() {
+		return this.actions[0];
+	}
 
+	public ArrayList<Action> getFlopActions() {
+		return this.actions[1];
+	}
+	public ArrayList<Action> getTurnActions() {
+		return this.actions[2];
+	}
+	public ArrayList<Action> getRiverActions() {
+		return this.actions[3];
+	}
 }
