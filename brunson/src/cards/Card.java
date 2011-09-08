410a44b3d4ecc0db37671e774c5474e052a936c2
Liei <tormunds@stud.ntnu.no>
2011-09-08 18:34:48 +0200

    Removed deprecated code, updated tests.

--- brunson/src/cards/Card.java
+++ brunson/src/cards/Card.java
@@ -5,8 +5,7 @@ public class Card implements Comparable<Card>{
 	private Value value;
 	private Suit suit;	
 	
-	
-	public Card(Value value, Suit suit){
+	Card(Value value, Suit suit){
 		this.value = value;
 		this.suit = suit;
 	}
@@ -22,7 +21,8 @@ public class Card implements Comparable<Card>{
 	public String toString(){
 		return String.format("%s%s",value,suit);
 	}
-	
+
+	@Deprecated
 	public int getIntValue() {
 		for(int i = 0; i< Value.values().length; i++) {
 			if(this.value == Value.values()[i]) {
