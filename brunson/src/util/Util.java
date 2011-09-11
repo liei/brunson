a1802e9202d3e14388a5598e95b0f1671520c8f2
Liei <tormunds@stud.ntnu.no>
2011-09-11 17:29:25 +0200

    Added Util. It only has cyclePlayers(...)
    cyclePlayers return a cyclic Iterator that will iterate eternally
    through the list given to it. We can remove players from it by calling
    remove() on the Iterator.

---
+++ brunson/src/util/Util.java
@@ -0,0 +1,62 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import player.Player;
+
+public class Util {
+	
+	public static void main(String[] args) {
+		List<Player> list = new ArrayList<Player>();
+		
+		Iterator<Player> it = new PlayerCycler(list,0);
+		Random r = new Random();
+		while(it.hasNext()){
+			Player p = it.next();
+			System.out.println(p);
+			if(r.nextBoolean()){
+				it.remove();
+				System.out.printf("removed %s%n",p);
+			}
+		}
+	}
+
+	static class PlayerCycler implements Iterator<Player>,Iterable<Player>{
+	
+		private List<Player> list;
+		int index;
+		boolean hasCurrent;
+		
+		
+		public PlayerCycler(List<Player> list,int index){
+			this.list = list;
+			this.index = index == 0 ? list.size() - 1 : index - 1;
+			hasCurrent = false;
+		}
+		
+		public boolean hasNext() {
+			return list.size() > 0;
+		}
+	
+		public Player next() {
+			index = (index + 1) % list.size();
+			hasCurrent = true;
+			return  list.get(index);
+		}
+	
+		public void remove() {
+			if(!hasCurrent)
+				throw new IllegalStateException();
+			list.remove(index--);
+			hasCurrent = false;
+		}
+	
+		public Iterator<Player> iterator() {
+			return this;
+		}
+	}
+}
