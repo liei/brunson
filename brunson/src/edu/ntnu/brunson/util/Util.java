19dfd7298affae4ce5a351d5da3932f59594ab9b
Tormund Haus <tormundhaus@gmail.com>
2011-09-14 15:14:14 +0200

    Added Util class.

---
+++ brunson/src/edu/ntnu/brunson/util/Util.java
@@ -0,0 +1,23 @@
+package edu.ntnu.brunson.util;
+
+import java.util.Random;
+
+public class Util {
+
+	public static final Random RNG = new Random();
+	
+	/**
+	 * returns a true chance % of the time, uses Util.RNG to get randomness.
+	 * @param chance, the chance of returning true, [0,100]
+	 *        if chance is < 0 or > 100 throws an IllegalArgumentException()
+	 * @return will be true chance %
+	 *         always returns false if chance == 0
+	 *         always returns true if chance == 100
+	 */
+	public static boolean randomBoolean(int chance) {
+		return RNG.nextInt(100) + 1 <= chance;
+	}
+	
+	
+	
+}
