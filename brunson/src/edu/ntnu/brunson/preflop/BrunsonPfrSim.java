5931d3cf5e413e268364e1fcbeaeac8ac8a326a8
Liei <tormunds@stud.ntnu.no>
2011-09-24 13:10:12 +0200

    Finished and refactored PreFlop code. The interface should be PreFlopSimulation.generate(...) to generate a file, PreFlopTable.load(...) to load a file, giving the number of players. And PreFlopTable.get(Pile hand) and PreFlopTable.inPercentile(Pile hand, int percentile) to get info from PreFlopTable instance.

--- brunson/src/edu/ntnu/brunson/preflop/BrunsonPfrSim.java
+++ brunson/src/edu/ntnu/brunson/preflop/BrunsonPfrSim.java
@@ -1,43 +1,60 @@
 package edu.ntnu.brunson.preflop;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.Option;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
 
 public class BrunsonPfrSim {
 	
-	private static final String USAGE = "java -jar brunson-pfrsim.jar [-p <players>] [-t <timeout>] [-r <runs>] [-o <outputfile>]";
+	private static final String USAGE = "java -jar brunson-pfrsim.jar [-p <players>] [-t <timeout>] [-r <runs>] [-w <workers>] [-o <outputfile>]";
 
 	private static final Options  options = new Options();
 	
 	static {
 		options.addOption("p","players",true,"Max players to simulate to. 10 is default.")
-			.addOption("t","timeout",true,"Time to let the program run before closing, in hours.24 is default.")
+			.addOption("t","timeout",true,"Time to let the program run before closing, in hours. 24 is default.")
 			.addOption("r","runs",true,"How many runs to to with each hand. 1000 is default.")
-			.addOption("o","ofile",true,"File to put output, stdout chosen as default.");
+			.addOption("o","ofile",true,"File to put output, stdout chosen as default.")
+			.addOption("w","workers",true,"How many worker threads to have. 4 is default");
 	}
 	
 	public static void main(String[] args) {
-		for(String s : args)
-			System.out.println(s);
-		
-		CommandLineParser clp = new BasicParser();
 		CommandLine cl = null;
+		CommandLineParser clp = new BasicParser();
 		try {
 			cl = clp.parse(options,args);
-			System.out.println("testing");
-			for(String s : cl.getArgs()){
-				System.out.println(s);
-			}
 		} catch (ParseException e) {
 			e.printStackTrace();
-			System.out.println(USAGE);
+			HelpFormatter hf = new HelpFormatter();
+			hf.printHelp(USAGE,options);
 			System.exit(1);
 		}
+		
+		int players = Integer.parseInt(cl.getOptionValue('p',"10")); 
+		int timeout = Integer.parseInt(cl.getOptionValue('t',"24"));
+		int runs = Integer.parseInt(cl.getOptionValue('r',"1000"));
+		int workers = Integer.parseInt(cl.getOptionValue('w',"4"));
+		
+		PrintStream out;
+		if(cl.hasOption('o')){
+			FileOutputStream fos = null;
+			try {
+				fos = new FileOutputStream(cl.getOptionValue('o'));
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			out = new PrintStream(fos);
+		} else {
+			out = System.out;
+		}
+		PreFlopSimulation.generate(out,players,timeout,runs,workers);
 	}
 }
---
+++ brunson/src/edu/ntnu/brunson/preflop/PreFlopTable.java
@@ -0,0 +1,94 @@
+package edu.ntnu.brunson.preflop;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.ntnu.brunson.cards.Card;
+import edu.ntnu.brunson.cards.Deck;
+import edu.ntnu.brunson.cards.Pile;
+import edu.ntnu.brunson.util.PileUtil;
+
+public class PreFlopTable {
+	
+	private Map<String, Integer> table;
+	
+	public static void main(String[] args) {
+		try {
+			PreFlopTable pft = load("preflop2.txt",3);
+			for(Pile pile : PileUtil.combinations(Deck.fullDeck(),2))
+				System.out.printf("%s : %d%n",pile,pft.get(pile));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private PreFlopTable(){
+		table = new HashMap<String,Integer>(); 
+	}
+	
+	public static PreFlopTable load(String filepath, int players) throws IOException, ParseException{
+
+		BufferedReader br = null; 
+
+		br = new BufferedReader(new FileReader(filepath));
+		String line;
+		int lineno = 1;
+		while((line = br.readLine()) != null){
+			if(line.length() > 0 && line.charAt(0) != '#'){
+				try {
+					String[] toks = line.split("\\|");
+					if(toks.length != 2){
+						for(String s : toks)
+							System.out.println(s);
+						throw new Exception();
+					}
+					if(players == Integer.parseInt(toks[0])){
+						return loadTable(toks[1].split("\\s+"));
+					}
+				} catch (Exception e) {
+					throw new ParseException(line,lineno);
+				}
+			}
+			lineno++;
+		}
+		return null;
+	}
+
+	private static PreFlopTable loadTable(String[] hands) throws Exception {
+		PreFlopTable pft = new PreFlopTable();
+		for(String s : hands){
+			String[] toks = s.split(":");
+			if(toks.length != 2)
+				throw new Exception();
+			pft.table.put(toks[0],Integer.parseInt(toks[1]));
+		}
+		return pft;
+	}
+	
+	static String equivClass(Pile hand){
+		if(hand.size() != 2)
+			throw new IllegalArgumentException("hand must have size 2");
+		hand.sort();
+		Card c1 = hand.getCard(0);
+		Card c2 = hand.getCard(1);
+		boolean suited = c1.getSuit() == c2.getSuit();
+		return String.format("%s%s%s",c1.getValue(),c2.getValue(),suited ? "s" : "");
+	}
+	
+	public int get(Pile hand){
+		String eqClass = equivClass(hand);
+		return table.get(eqClass);
+	}
+	
+	public boolean inPercentile(Pile hand, int percentile){
+		return get(hand) >= percentile;
+	}
+}
