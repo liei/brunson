148e7dcc6fc397075b78134d278dbf49da891e83
Liei <tormunds@stud.ntnu.no>
2011-09-25 11:35:12 +0200

    Fixing output.

--- brunson/src/edu/ntnu/brunson/manager/Output.java
+++ brunson/src/edu/ntnu/brunson/manager/Output.java
@@ -5,49 +5,57 @@ import java.util.Iterator;
 
 public class Output {
 	
+	public static boolean debug = false;
+	
 	private static ArrayList<String> handHistory = new ArrayList<String>();
-	private static ArrayList<String> debug = new ArrayList<String>();
+	//private static ArrayList<String> debug = new ArrayList<String>();
 	private static boolean verbose = true;
 	
 	private Output(){
 		
 	}
-	
-	public static void println(String line){
-		System.out.println(line);
+	public static void println() {
+		System.out.println();
 	}
-	
-	public static void print(String line){
-		System.out.print(line);
+	public static void println(Object o){
+		System.out.println(o);
+	}
+
+	public static void print(Object o){
+		System.out.print(o);
 	}
 	
 	public static void printf(String line,Object... os){
 		System.out.printf(line,os);
 	}
 	
-	public static void addToHH(String s) {
-		handHistory.add(s);
-	}
-	
-	public static void addToDebug(String s) {
-		debug.add(s);
-	}
-	
-	public static void printHH() {
-		if(!verbose) {
-			handHistory.clear();
-			return;
-		}
-		Iterator<String> iterator = handHistory.iterator();
-		while(iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
-		handHistory.clear();
+	public static void debugf(String line,Object... os){
+		if(debug)
+			System.out.printf(line,os);
 	}
 
-	public static void debugf(String line,Object... os){
-		System.out.printf(line,os);
-	}
+	
+//	public static void addToHH(String s) {
+//		handHistory.add(s);
+//	}
+//	
+//	public static void addToDebug(String s) {
+//		debug.add(s);
+//	}
+//	
+//	public static void printHH() {
+//		if(!verbose) {
+//			handHistory.clear();
+//			return;
+//		}
+//		Iterator<String> iterator = handHistory.iterator();
+//		while(iterator.hasNext()) {
+//			System.out.println(iterator.next());
+//		}
+//		handHistory.clear();
+//	}
+
+
 	
 	
 }
